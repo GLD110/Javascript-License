f66a20867dbd469d42ff39a6fb9dd802e74b1f24
diana4545 <diana4545@outlook.com>
2019-05-04 17:27:09 +0100

    manage

--- application/controllers/collection.php
+++ application/controllers/collection.php
@@ -24,33 +24,33 @@ class Collection extends MY_Controller {
       $this->manage();
   }
 
-  public function manage( $page =  0 ){
-    // Check the login
-    $this->is_logged_in();
-
-    // Init the search value
-    $this->initSearchValue();
-
-    $arrCondition =  array(
-         'title' => $this->_searchVal['title'],
-         'page_number' => $page,
-         'page_size' => $this->_searchVal['page_size'],
-         'sort' => $this->_searchVal['sort_field'] . ' ' . $this->_searchVal['sort_direction'],
-    );
-    $data['query'] =  $this->Collection_model->getList( $arrCondition );
-    $data['total_count'] = $this->Collection_model->getTotalCount();
-    $data['page'] = $page;
-
-    // Define the rendering data
-    $data = $data + $this->setRenderData();
-
-    // Load Pagenation
-    $this->load->library('pagination');
-
-    $this->load->view('view_header');
-    $this->load->view('view_collection', $data );
-    $this->load->view('view_footer');
-  }
+  // public function manage( $page =  0 ){
+  //   // Check the login
+  //   $this->is_logged_in();
+  //
+  //   // Init the search value
+  //   $this->initSearchValue();
+  //
+  //   $arrCondition =  array(
+  //        'title' => $this->_searchVal['title'],
+  //        'page_number' => $page,
+  //        'page_size' => $this->_searchVal['page_size'],
+  //        'sort' => $this->_searchVal['sort_field'] . ' ' . $this->_searchVal['sort_direction'],
+  //   );
+  //   $data['query'] =  $this->Collection_model->getList( $arrCondition );
+  //   $data['total_count'] = $this->Collection_model->getTotalCount();
+  //   $data['page'] = $page;
+  //
+  //   // Define the rendering data
+  //   $data = $data + $this->setRenderData();
+  //
+  //   // Load Pagenation
+  //   $this->load->library('pagination');
+  //
+  //   $this->load->view('view_header');
+  //   $this->load->view('view_collection', $data );
+  //   $this->load->view('view_footer');
+  // }
 
   public function sync()
   {
